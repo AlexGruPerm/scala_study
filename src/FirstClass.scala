fa5053270b55e9996b220d1c0d5d3682654889ad
AlexGruPerm <ugr@bk.ru>
2018-05-14 10:21:03 +0500

    ready to begin Project and git

--- src/FirstClass.scala
+++ src/FirstClass.scala
@@ -1,7 +1,3 @@
-class FirstClass {
-
-  // https://docs.scala-lang.org/getting-started-intellij-track/getting-started-with-scala-in-intellij.html
-
-  println("Begin")
-
+object FirstClass extends App{
+  println("Begin FirstClass xxx")
 }
