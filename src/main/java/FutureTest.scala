8b5ad8680e0a20cad5e7fb9b164c15ddd874c244
AlexGruPerm <ugr@bk.ru>
2018-06-15 10:34:05 +0500

    FROM WORK: Begin read "Prokopec, Aleksandar - Learning Concurrent Programming in Scala (2017, Packt Publishing)" #2

--- src/main/java/FutureTest.scala
+++ src/main/java/FutureTest.scala
@@ -1,99 +1,129 @@
 import scala.concurrent.{Future, Await}
 import scala.concurrent.duration._
-import scala.util.{Success, Failure}
+import scala.util.{Success, Failure, Try}
 import scala.concurrent.ExecutionContext.Implicits.global
 
-object FutureTest extends App {
-  println("App")
-
-
-
 /*
-  def slowFunc_2(toVal : Int) = {
-    Thread.sleep(600)
-    for (i <- 11 to toVal) yield i
-  }
-*/
+*DOCS:
+* 1) https://habr.com/post/233555/
+* 2) https://viktorklang.com/blog/Futures-in-Scala-2.12-part-5.html
+*    http://groz.github.io/scala/intro/futures/
+*    https://docs.scala-lang.org/sips/completed/futures-promises.html
+*    https://stackoverflow.com/questions/25056957/why-future-sequence-executes-my-futures-in-parallel-rather-than-in-series
+*
+**/
 
-  def getFuture3 = {
-    Thread.sleep(700)
-    Future {
-      ((for (i <- 21 to 30) yield i).sum,3)
-    }
-  }
-
-  def getFuture4 = {
-    Thread.sleep(900)
-    Future {
-      ((for (i <- 31 to 40) yield i).sum,4)
-    }
-  }
-
-  def slowFunc_1 = {
-    Thread.sleep(1800)
-    for (i <- 1 to 10) yield i
-  }
-
-
-
+object FutureTest extends App {
   /*
-  val sumF2 = Future {
-    slowFunc_2(200).sum // set external data into Future
-  }
-*/
-  val v1 = math.round(math.random()*10)
-  val v2 = math.round(math.random()*10)
-
-  println(v1+" - "+v2)
-
-
-  val sumF1 = Future {(slowFunc_1.sum,1)}
-  val sumF = if (v1 > v2) getFuture3 else getFuture4
-
-  /*
-  sumF1 onComplete  {
-    case s => println("(1)"+s)
+  def getF(fNum : Int) = {
+    val sleepDuration = math.round(math.random()*100)
+    Thread.sleep(sleepDuration)
+    val f = Future {
+      (fNum, 10)
+    }
+    f
   }
 
-  sumF2 onComplete  {
-    case s => println("(2)"+s)
+  val seqFuts = for(i <- 2 to 10) yield {
+    getF(i).onComplete {
+      case Success(sRes: (Int,Int)) => println(" Future:"+sRes._1+" Value:"+sRes._2)
+      case Failure(fRes) => println(fRes)
+    }
   }
   */
 
+
+  val successors = Map(1 -> 2, 2 -> 3, 3 -> 4)
+
+  println("successors.get(5)"+successors.get(5))
+
+  successors.get(5) match {
+    case Some(n) => println(s"Successor is: $n")
+    case None => println("Could not find successor.")
+  }
+
+  (1==2) match {
+    case true  => println("equal")
+    case false => println("not equal")
+  }
+
+
+  //Using match to determine how many arguments take function
+
+
+
+
+  val f1a  = (a1: Int) => a1*2
+  val f2a  = (a1: Int, a2: Int) => a1+a2
+  val f2ad  = (a1: Int, a2: Int) => a1+a2*0.34
+
+  val f=f2a
+
+  val res = f(1,10)
+  println("res = "+res+" withType="+res.getClass.getName)
+
+
+  val l = f//(f,res)
+
+  l match {
+    case _:Function1[Int,Int] && 1==1 => println("This is a function of [Int] return Int")
+  }
+
+/*
+x match {
+   case _:MyFirst | _:MySecond => doSomething(x) // just use x instead of aOrB
+   case _ => doSomethingElse(x)
+}
+*/
+
+/*
+    case _:Function1[Int, Int] =>
+    case _:Function2[Int, Int, Int] =>
+    case _:Function2[Int, Int, Double] =>
+*/
+
+  /*
+  f match {
+    case _:(Int => Int)       => println("This is a function of 1 argument")
+    case _:((Int,Int) => Int) => println("This is a function of 2 arguments")
+    case _:AnyRef =>   println("Something else")
+  }
+  */
+
+  /*
+  def f(x: Any): String = x match {
+    case i:Int => "integer: " + i
+    case _:Double => "a double"
+    case s:String => "I want to say " + s
+  }
+  */
+
+
+  println(">>>")
+  Thread.sleep(1000)
+  println("<<<")
+
+
+}
+
+  //val sumF1 = Future {(slowFunc_1.sum,1)}
+  //val sumF = if (v1 > v2) getFuture3 else getFuture4
+
   //Соответственно,
-  // в случае успеха ваша функция обратного вызова будет вызывана с объектом класса Success
+  // в случае успеха ваша функция обратного вызова onComplete будет вызывана с объектом класса Success
   // , а в случае ошибки - Failure.
+  /*
   sumF onComplete {
     //case s => println("  Future:"+s.get._2+" Value:"+s.get._1)
     case Success(result: (Int,Int)) => println("  Future:"+result._2+" Value:"+result._1)
     case Failure(ex: Exception) => println(ex)
   }
-
-  sumF1 onComplete {
-    //case s => println("  Future:"+s.get._2+" Value:"+s.get._1)
-    case Success(result: (Int,Int)) => println("  Future:"+result._2+" Value:"+result._1)
-    case Failure(ex: Exception) => println(ex)
-  }
-
- // sumF.foreach(x => println(x))
-
-  val result = Future.sequence(Seq(sumF, sumF1))
-
-  println(">>>")
-  Thread.sleep(3000)
-  println("<<<")
+  */
 
   /*
-  *DOCS:
-  * 1) https://habr.com/post/233555/
-  * 2) https://viktorklang.com/blog/Futures-in-Scala-2.12-part-5.html
-  * http://groz.github.io/scala/intro/futures/
-  * https://docs.scala-lang.org/sips/completed/futures-promises.html
-  * https://stackoverflow.com/questions/25056957/why-future-sequence-executes-my-futures-in-parallel-rather-than-in-series
-  *
-  **/
+  val result = Future.sequence(Seq(getF1, getF2))
 
-
-
-
-}
+  println(">>>")
+  Thread.sleep(1000)
+  println("<<<")
+  */
