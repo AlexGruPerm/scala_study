1eb12287d5f7c44ab065450ae538f9fd25c6913d
AlexGruPerm <4872931alex#>
2018-06-17 19:43:27 +0500

    Parquet reader - org.apache.parquet.io.ParquetDecodingException: Can not read value at 1 in block 0 in file file:

--- src/main/java/ParuqetTests.scala
+++ src/main/java/ParuqetTests.scala
@@ -1,10 +1,15 @@
 import com.sksamuel.avro4s.RecordFormat
+import org.apache.avro.generic.GenericRecord
 import org.slf4j.LoggerFactory
 import org.apache.hadoop.fs.Path
 import org.apache.avro.{Schema, SchemaBuilder}
-//import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.conf.Configuration
+import org.apache.parquet.avro.AvroParquetReader
 import org.apache.parquet.avro.AvroParquetWriter
-import org.apache.parquet.hadoop.{ParquetFileWriter, ParquetWriter}
+import org.apache.parquet.format.converter.ParquetMetadataConverter
+import org.apache.parquet.hadoop.util.HadoopInputFile
+import org.apache.parquet.hadoop.{ParquetFileWriter, ParquetReader, ParquetWriter}
+import org.apache.parquet.io.{InputFile, SeekableInputStream}
 
 case class Person(id:Int,name: String)
 
@@ -25,6 +30,26 @@ object ParuqetTests extends App {
       writer.close()
   }
 
+
+  def readFromFile(schema: Schema, path: String) = {
+
+    val configuration = new Configuration
+    val file = HadoopInputFile.fromPath(new Path(path), configuration);
+
+    val reader = AvroParquetReader.builder[Person](file).build().asInstanceOf[ParquetReader[Person]]
+
+    val format = RecordFormat[Person]
+
+    //val iter = Iterator.continually(reader.read).takeWhile(_ != null).map(format.from)
+
+    Iterator.continually(reader.read).takeWhile(_ != null).foreach(x => println(x.getClass.getName))
+    /*
+    val iter = Iterator.continually(reader.read).takeWhile(_ != null).map(x => format.from(x.asInstanceOf[GenericRecord]).asInstanceOf[Person])
+    val list = iter.toList
+    list
+    */
+  }
+
   val schema = SchemaBuilder
     .record("Person")
     .fields()
@@ -32,7 +57,10 @@ object ParuqetTests extends App {
     .requiredString("name")
     .endRecord()
 
-  val personData = for(i <- 1 to 1000000) yield {new Person(i,(i*10).toString)}
+    //val personData = for(i <- 1 to 1000000) yield {new Person(i,(i*10).toString)}
+    //writeToFile(personData,schema,"prq/test.parquet")
 
-    writeToFile(personData,schema,"prq/test.parquet")
+     readFromFile(schema,"prq/test.parquet")
+
+
 }
