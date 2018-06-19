133a3c555736f84348da0ecd5c624c241d4e780b
AlexGruPerm <ugr@bk.ru>
2018-06-19 23:00:27 +0500

    spark in src

--- src/main/java/MatchExpr.scala
+++ src/main/java/MatchExpr.scala
@@ -1,7 +1,7 @@
 object MatchExpr extends App {
 
   //1) simple String match
-  val firstArg = "z"
+  val firstArg = "y"
   val res = firstArg match {
     case "x" => "x--->"
     case "y" => "y----->"
@@ -9,6 +9,63 @@ object MatchExpr extends App {
   }
   println(firstArg+" - "+res)
 
-  //2)
+  //
+
+  val f = (_: Int) + (_: Int)
+  println(f(3,4))
+
+
+  def sum(a: Int, b: Int, c: Int) =
+    if (!b.isNaN) a + b + c
+    else a+c
+
+
+  def b(f: (Int,Int,Int) => Int, params:(Int,Int,Int)): Int = {
+    f(params._1, params._2, params._3)
+  }
+
+  println("bRes="+b(sum,(10,20,30)))
+
+  println("================================")
+
+  def mv1f = {
+    10
+  }
+
+  def mv2f = {
+    20
+  }
+
+  var mv = 2
+
+  val moreF = mv match {
+    case 1 => mv1f
+    case 2 => mv2f
+  }
+
+  val addMore = (x: Int) => x + moreF
+
+  println("addMore="+addMore(100).toString())
+
+  mv = 1
+  println("addMore="+addMore(100).toString())
+
+
+  case class Person(id:Int,name:String)
+
+  def listPersons(persons : Person*): Unit ={
+    println("persons.getClass.getName="+persons.getClass.getName)
+    persons.foreach(p => println(p))
+  }
+
+  val arr :Seq[Person] = Seq(new Person(1,"Ben"),new Person(2,"john"))
+
+  listPersons(arr: _*)
+
+  def printTime(out: java.io.PrintStream = Console.out) = {
+    out.println("time = " + System.currentTimeMillis())
+  }
+
+  printTime(Console.err)
 
 }
--- src/main/java/ParuqetTests.scala
+++ src/main/java/ParuqetTests.scala
@@ -4,6 +4,7 @@ import org.slf4j.LoggerFactory
 import org.apache.hadoop.fs.Path
 import org.apache.avro.{Schema, SchemaBuilder}
 import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.io.{IntWritable, WritableComparable}
 import org.apache.parquet.avro.AvroParquetReader
 import org.apache.parquet.avro.AvroParquetWriter
 import org.apache.parquet.format.converter.ParquetMetadataConverter
@@ -11,7 +12,20 @@ import org.apache.parquet.hadoop.util.HadoopInputFile
 import org.apache.parquet.hadoop.{ParquetFileWriter, ParquetReader, ParquetWriter}
 import org.apache.parquet.io.{InputFile, SeekableInputStream}
 
-case class Person(id:Int,name: String)
+//case class Person(id:Int,name: Int)
+
+/*
+
+  object Person {
+    def id: Int = 0
+    def name: Int = 0
+    def apply(id:Int,name: Int): Person = new Person(id, name)
+  }
+
+*/
+
+case class Person(id:Int,name: Int)
+
 
 object ParuqetTests extends App {
   val logger = LoggerFactory.getLogger(this.getClass)
@@ -31,18 +45,51 @@ object ParuqetTests extends App {
   }
 
 
-  def readFromFile(schema: Schema, path: String) = {
+  def readFromFile(path: String) = {
 
-    val configuration = new Configuration
-    val file = HadoopInputFile.fromPath(new Path(path), configuration);
+    val conf = new Configuration
+    val file = HadoopInputFile.fromPath(new Path(path), conf);
 
-    val reader = AvroParquetReader.builder[Person](file).build().asInstanceOf[ParquetReader[Person]]
+/*
+BASIC EXAMPLE -------------------------------------
 
-    val format = RecordFormat[Person]
+val reader = AvroParquetReader.builder[GenericRecord](path).build().asInstanceOf[ParquetReader[GenericRecord]]
 
-    //val iter = Iterator.continually(reader.read).takeWhile(_ != null).map(format.from)
+val iter = Iterator.continually(reader.read).takeWhile(_ != null)
 
-    Iterator.continually(reader.read).takeWhile(_ != null).foreach(x => println(x.getClass.getName))
+val list = iter.toList
+
+case class Bibble(name: String, location: String)
+
+val format = RecordFormat[Bibble]
+
+val bibble = format.from(record)
+
+
+val reader = AvroParquetReader.builder[GenericRecord](path).build().asInstanceOf[ParquetReader[GenericRecord]]
+val format = RecordFormat[Bibble]
+// iter is now an Iterator[Bibble]
+val iter = Iterator.continually(reader.read).takeWhile(_ != null).map(format.from)
+// and list is now a List[Bibble]
+val list = iter.toList
+*/
+
+   // val reader = AvroParquetReader.builder[GenericRecord](file).build().asInstanceOf[ParquetReader[GenericRecord]]
+
+    val reader = AvroParquetReader.builder[GenericRecord](file).build()//.asInstanceOf[ParquetReader[GenericRecord]]
+
+    //val format = RecordFormat[Person]
+    //val iter = Iterator.continually(reader.read).takeWhile(_ != null).map(x =>  format.from(x.asInstanceOf[GenericRecord]))//.map(x => format.from(x.asInstanceOf[GenericRecord])).toList
+
+    var r: GenericRecord = reader.read()
+
+    //val iter = Iterator.continually(reader.read).takeWhile(_ != null).toList
+    //println("iter.size="+iter.size)
+
+
+    //.foreach(r => println(r))
+
+    //Iterator.continually(reader.read)/*.takeWhile(_ != null)*/.foreach(x => println("1")/*x.getClass.getName)*/)
     /*
     val iter = Iterator.continually(reader.read).takeWhile(_ != null).map(x => format.from(x.asInstanceOf[GenericRecord]).asInstanceOf[Person])
     val list = iter.toList
@@ -50,17 +97,30 @@ object ParuqetTests extends App {
     */
   }
 
+
+
+
+/*
   val schema = SchemaBuilder
     .record("Person")
     .fields()
-    .requiredInt("id")
-    .requiredString("name")
+    .optionalInt("id")
+    .optionalInt("name")
     .endRecord()
+  val personData = for(i <- 1 to 1000000) yield {new Person(i,123.toInt)}
+  writeToFile(personData,schema,"prq/test.parquet")
+*/
 
-    //val personData = for(i <- 1 to 1000000) yield {new Person(i,(i*10).toString)}
-    //writeToFile(personData,schema,"prq/test.parquet")
+/*
+  val schemaRead = SchemaBuilder
+    .record("Person")
+    .fields()
+    .optionalInt("id")
+    .optionalInt("name")
+    .endRecord()
+*/
 
-     readFromFile(schema,"prq/test.parquet")
+  readFromFile(/*schemaRead,*/"prq/test.parquet")
 
 
 }
